9a5aa379fd010c0d230a06e5f3db4de2ee1521b4
AriPurwoAji <ajiaripurwo@gmail.com>
2026-04-15 15:29:48 +0700

    fiks

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "Ari Purwo Aji!",
+		})
+	})
+
+	r.GET("/api/user", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"id":   1,
+			"nama": "Ari",
+			"role": "admin",
+		})
+	})
+
+	r.Run(":8080")
+}
